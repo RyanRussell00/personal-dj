055d0f2fc57189b3b432344f8c5689eeb4304a11
Ryan Russell <ryan@russell.software>
2023-11-22 18:54:51 -0800

    Updated CORS

--- api/app.js
+++ api/app.js
@@ -52,7 +52,7 @@ app
 
 // Preflight
 app.use(function (req, res, next) {
-    const allowedOrigins = ['http://localhost:3000', 'https://personaldj.net'];
+    const allowedOrigins = ['http://localhost:3000', 'https://www.personaldj.net'];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header("Access-Control-Allow-Origin", origin);
